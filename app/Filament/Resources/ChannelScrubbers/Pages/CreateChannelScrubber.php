cf9e2ea46faac51a035f6f5ac80efadcb75a4faf
Shaun Parkison <shparkison@gmail.com>
2026-03-24 15:31:14 -0600

    chore: Remove mutate since using model actions in service provider
    This method isn't used when creating via modal so it's unused anyway. Remove it since it's dead code.

--- app/Filament/Resources/ChannelScrubbers/Pages/CreateChannelScrubber.php
+++ app/Filament/Resources/ChannelScrubbers/Pages/CreateChannelScrubber.php
@@ -4,18 +4,8 @@ namespace App\Filament\Resources\ChannelScrubbers\Pages;
 
 use App\Filament\Resources\ChannelScrubbers\ChannelScrubberResource;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 
 class CreateChannelScrubber extends CreateRecord
 {
     protected static string $resource = ChannelScrubberResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $data['user_id'] = Auth::id();
-        $data['uuid'] = Str::uuid()->toString();
-
-        return $data;
-    }
 }
